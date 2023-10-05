87b05aa4617f96cd4348e80aab949096f86f93eb
Irnes <duran.irnes@gmail.com>
2023-10-05 15:35:21 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React from 'react'
+import './App.css'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
-import HomePage from './pages/Homepage/Homepage';
-import ItemPage from './pages/ItemPage/ItemPage';
+import HomePage from './pages/Homepage/Homepage'
+import ItemPage from './pages/ItemPage/ItemPage'
 
-import Navbar from './components/Navbar/Navbar.tsx';
-
+import Navbar from './components/Navbar/Navbar.tsx'
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      test
+      <Navbar />
       <Switch>
-        <Route exact path='/' render={ () => (<Redirect to ='/tv-shows'/>) }/>
-        <Route path='/movies' component={HomePage}/>
-        <Route path='/tv-shows' component={HomePage}/>
-        <Route path='/:item' component={ItemPage} />
+        <Route exact path="/" render={() => <Redirect to="/tv-shows" />} />
+        <Route path="/movies" component={HomePage} />
+        <Route path="/tv-shows" component={HomePage} />
+        <Route path="/:item" component={ItemPage} />
       </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
